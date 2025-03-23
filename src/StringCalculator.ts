feca89d68df780ac513717e41065fa0206262dac
dddivyadwivedi <dddivya7980@gmail.com>
2025-03-23 18:40:32 +0530

    Written code to return sum when comma separated values are passed

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,11 +1,16 @@
 export const StringCalculator = (numbers: string): number => {
-    // Check for empty string and return 0
+    //Case 1: Check for empty string and return 0
     if (numbers === '') {
         return 0;
     }
-    // Check for single single number and return the number itself
+    //Case 2: Check for single single number and return the number itself
     if (numbers.length === 1) {
         return parseInt(numbers);
     }
-    return 0;
+    //Case 3: Split the numbers by comma and return the sum
+    let delimiters = ',';
+    // Convert string to number array, ignoring invalid numbers
+    const numArray = numbers.split(delimiters).map((num) => parseInt(num, 10)).filter((n) => !isNaN(n));
+    // Return the sum of the numbers
+    return numArray.reduce((sum, num) => sum + num, 0);
 }
