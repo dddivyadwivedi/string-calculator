db23b7748c48a92a8551513851991795a3bb4cc1
dddivyadwivedi <dddivya7980@gmail.com>
2025-03-23 18:17:12 +0530

    Added test case for single number and added readme file with screenshots of empty string test case passed

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,3 +1,7 @@
 export const StringCalculator = (numbers: string): number => {
+    // Check for empty string and return 0
+    if (numbers === '') {
+        return 0;
+    }
     return 0;
 }
--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -2,7 +2,10 @@ import { describe, expect, it } from "vitest";
 import { StringCalculator } from '../src/StringCalculator'
 
 describe('StringCalculator', () => {
-    it('It should return 0 for an empty string', () => {
+    it('It should return 0 for an empty string parameter', () => {
         expect(StringCalculator('')).toBe(0);
     });
+    it('It should return the number itself if paramater passed is single number', () => {
+
+    })
 });
