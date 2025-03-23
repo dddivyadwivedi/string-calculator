119abf68ae12f3e0a3d5a5ca7cff57489af15170
dddivyadwivedi <dddivya7980@gmail.com>
2025-03-23 18:28:21 +0530

    Added test case for two comma separated numbers should return sum

--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -8,4 +8,8 @@ describe('StringCalculator', () => {
     it('It should return the number itself if paramater passed is single number', () => {
         expect(StringCalculator('1')).toBe(1);
     })
+    it('It should return the sum of two comma separated numbers', () => {
+        expect(StringCalculator('9,2')).toBe(11);
+        expect(StringCalculator('50,1')).toBe(51);
+    })
 });
